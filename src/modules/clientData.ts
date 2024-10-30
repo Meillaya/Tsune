c4ffe49cdc809ebbcade2f43229f28d5d2f78186
Nathan Agbomedarho <nathanagbomed@proton.me>
2024-10-30 08:53:18 -0400

    Delete mistake...

--- src/modules/clientData.ts
+++
@@ -1,7 +0,0 @@
-import { ClientData } from '../types/types';
-
-export const clientData: ClientData = {
-    clientId: 22023,
-    redirectUri: "https://anilist.co/api/v2/oauth/pin",
-    clientSecret: 'TEIMqkMsGTEHP6KTE7aZjfjfEmRksnJgvFVruZCD',
-};
