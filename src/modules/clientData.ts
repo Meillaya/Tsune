b9d8ac9472680a91073935543bd04bfb9f41d90b
Nathan Agbomedarho <nathanagbomed@proton.me>
2024-11-02 05:55:18 -0400

    again

--- src/modules/clientData.ts
+++ src/modules/clientData.ts
@@ -2,6 +2,6 @@ import { ClientData } from "../types/types";
 
 export const clientData: ClientData = {
   clientId: 22108,
-  redirectUri: 'https://tsune.vercel.app//auth/callback',
+  redirectUri: 'https://tsune.vercel.app/auth/callback',
   clientSecret: 'SXVsEkfa1hG0br0qC1uQjJCesB3CxjRnfVmq0KSq',
 };
